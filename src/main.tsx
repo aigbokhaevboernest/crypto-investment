0cb074e7c6b19e23bae0bf0ac4b8f1cbd75dcf2a
aigbokhaevboernest <129731110+aigbokhaevboernest@users.noreply.github.com>
2026-07-07 17:45:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,3 @@
-window.onerror = function (message, source, lineno, colno, error) {
-  alert(`Error: ${message}\nAt: ${source}:${lineno}:${colno}`);
-};
-window.addEventListener("unhandledrejection", (event) => {
-  alert(`Unhandled rejection: ${event.reason}`);
-});
-
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
